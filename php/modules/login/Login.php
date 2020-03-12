120bf2388441cfb1d7158791e99f17ce94dadc54
satthabut <satthabut.o@gmail.com>
2020-03-13 01:20:42 +0700

    add login

--- php/modules/login/Login.php
+++ php/modules/login/Login.php
@@ -1,14 +1,31 @@
 <?php
+include('../../database.php');
+session_start();
+
+$email = mysqli_real_escape_string($conn,$_POST['email']);
+$password = mysqli_real_escape_string($conn,$_POST['password']);
+$password = md5($password);
+
+$sql = "SELECT * FROM member WHERE email = $email AND password = $password";
+$result = mysqli_query($conn,$sql);
 
 $userLog = fopen("../../logs/login-log.log","a");
-print_r($userLog);
-  $text = date("Y-m-d H:m:s")."\t".
-          "\t".
-          $_SERVER['HTTP_HOST'].
-          $_SERVER['REQUEST_URI'].
-          "\t".get_client_ip()."\n";
+$text = date("Y-m-d H:m:s")."\t".
+        "\t".
+        $_SERVER['HTTP_HOST'].
+        $_SERVER['REQUEST_URI'].
+        " | ".$sql." | ".
+        "\t".get_client_ip()."\n";
   fwrite($userLog,$text);
   fclose($userLog);
+  header("location:../search/v_search.php");
+if(!$result){
+  echo "Email and Password Incorrect!";
+}else{
+  $_SESSION['UserID'] = $result['id'];
+  session_write_close();
+  header("location:../search/v_search.php");
+}
 function get_client_ip() {
     $ipaddress = '';
     if (getenv('HTTP_CLIENT_IP'))
--- php/modules/login/v_login.php
+++ php/modules/login/v_login.php
@@ -5,8 +5,8 @@
 <h2>Member management</h2>
   <form class="modal-content animate" action="Login.php" method="post">
     <div class="container">
-      <label for="username"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="username" required>
+      <label for="email"><b>Email</b></label>
+      <input type="email" placeholder="Enter Email" name="email" required>
       <label for="password"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="password" required>
       <button type="submit">Login</button>
